323ec5c03d0416e2665da6aab9b5b92bf39f1804
jakob <Jakobpstrobl@gmail.com>
2021-06-17 22:30:13 -0400

    Created cvars w/ on change callbacks: enabled, big demo chance, pad demo chance

--- boost-roulette/BoostRoulette.cpp
+++ boost-roulette/BoostRoulette.cpp
@@ -1,12 +1,80 @@
 #include "BoostRoulette.h"
+#include <stdio.h>
 
 BAKKESMOD_PLUGIN(BoostRoulette, "Boost Roulette", "0.1", 0)
 void BoostRoulette::onLoad() {
 	this->cvarManager->log("BoostRoulette::onLoad() was invoked!");
 
+	// Default Values
+	bool enabled = false;
+	float bigBoostDemoChance = 1.0f / NUM_BIG_BOOSTS;
+	float padDemoChance = 1.0f / NUM_PADS;
 
+	this->enabled = std::make_shared<CVarWrapper>(
+		this->cvarManager->registerCvar(
+			"boost_roulette_is_enabled",
+			std::to_string(enabled),
+			"Enables/disables Boost Roulette plugin.",
+			true,
+			true,
+			0.0,
+			true,
+			0.0,
+			false
+		)
+	);
+
+	this->bigBoostDemoChance = std::make_shared<CVarWrapper>(
+		this->cvarManager->registerCvar(
+			"boost_roulette_big_boost_chance",
+			std::to_string(bigBoostDemoChance),
+			"The probability a big boost demos on pickup.",
+			true,
+			true,
+			0.0,
+			true,
+			1.0,
+			false
+		)
+	);
+
+	this->padDemoChance = std::make_shared<CVarWrapper>(
+		this->cvarManager->registerCvar(
+			"boost_roulette_pad_chance",
+			std::to_string(padDemoChance),
+			"The probability a boost pad demos on pickup.",
+			true,
+			true,
+			0.0,
+			true,
+			1.0,
+			false
+		)
+	);
+
+
+	(*this->enabled).addOnValueChanged(std::bind(&BoostRoulette::onEnabledChanged, this, std::placeholders::_1, std::placeholders::_2));
+	(*this->bigBoostDemoChance).addOnValueChanged(std::bind(&BoostRoulette::onBoostDemoChanceChanged, this, std::placeholders::_1, std::placeholders::_2));
+	(*this->padDemoChance).addOnValueChanged(std::bind(&BoostRoulette::onBoostDemoChanceChanged, this, std::placeholders::_1, std::placeholders::_2));
 }
 
 void BoostRoulette::onUnload() {
 	this->cvarManager->log("Boost Roulette is being unloaded...");
+}
+
+void BoostRoulette::onEnabledChanged(std::string oldValue, CVarWrapper cvar) {
+	if (cvar.getBoolValue()) {
+		this->cvarManager->log("Boost Roulette enabled!");
+	}
+	else {
+		this->cvarManager->log("Boost Roulette disabled!");
+	}
+}
+
+void BoostRoulette::onBoostDemoChanceChanged(std::string oldValue, CVarWrapper cvar) {
+	const int MSG_SIZE = 50;
+	char msg[MSG_SIZE];
+	
+	int chars = snprintf(msg, MSG_SIZE, "%s => %.2f", cvar.getCVarName().c_str(), cvar.getFloatValue());
+	this->cvarManager->log(msg);
 }
--- boost-roulette/BoostRoulette.h
+++ boost-roulette/BoostRoulette.h
@@ -3,8 +3,25 @@
 #include "bakkesmod\plugin\bakkesmodplugin.h"
 
 class BoostRoulette : public BakkesMod::Plugin::BakkesModPlugin {
+	private:
+		std::shared_ptr<CVarWrapper> enabled;
+		std::shared_ptr<CVarWrapper> bigBoostDemoChance;
+		std::shared_ptr<CVarWrapper> padDemoChance;
+
 	public:
 		virtual void onLoad();
 		virtual void onUnload();
+
+		void onEnabledChanged(std::string oldValue, CVarWrapper cvar);
+		void onBoostDemoChanceChanged(std::string oldValue, CVarWrapper cvar);
+
+
 };
 
+enum BoostType {
+	BIG = 1,
+	PAD = 2,
+};
+
+const int NUM_BIG_BOOSTS = 6;
+const int NUM_PADS = 28;
